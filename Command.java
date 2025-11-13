8c9e8cac07f37ea7f6ef6241f6d7d399a7323ff9
gaurav <sononegaurav78@gmail.com>
2025-11-13 21:47:45 +0530

    Lastest changes

---
+++ Command.java
@@ -0,0 +1,10 @@
+class Command
+{
+  public static void main(String x[])
+{
+      double a=Double.parseDouble(x[0]);
+      double b=Double.parseDouble(x[1]);
+      double c=a+b;
+     System.out.println(c);
+}
+}
